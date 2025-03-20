295ac4e58079d24e60fc892a843ed5b31d817db1
Rafael Pastrana <arpj@princeton.edu>
2025-03-20 15:53:03 -0400

    Fiddling with TNA script

--- scripts/tna.py
+++ scripts/tna.py
@@ -18,6 +18,8 @@ from mayan_vaults.optimization import solve_thrust_minmax_vault
 
 from mayan_vaults.plotting import VaultPlotter
 from mayan_vaults.plotting import plot_thrust_minmax_vault
+from mayan_vaults.optimization import constraint_thrust_fn
+from mayan_vaults.optimization import calculate_start_params
 
 
 def run_tna_experiment():
@@ -25,14 +27,14 @@ def run_tna_experiment():
     TNA sandbox.
     """
     # load yaml file with hyperparameters
-    with open(f"tna.yml") as file:
+    with open("tna.yml") as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
 
     vault = create_vault(**config["vault"])
-    print(vault)
+    # print(vault)
 
     # Instantiate a topology diagram
-    topology = create_topology_from_vault(vault, px0=-3.7)
+    topology = create_topology_from_vault(vault, px0=-10.0)
     print(topology)
 
     # JAX CEM - form finding
@@ -53,42 +55,38 @@ def run_tna_experiment():
     print(f"Thrust at support: {thrust:.2f}")
     print(f"Ratio thrust / SW [%]: {100.0 * thrust / sw:.1f}")
 
-    print(f"\nLoads:")
-    for node in network.nodes():
-        print(f"\tNode {node}: {network.node_attribute(node, 'qy')}")
+    # Constraint check
+    params = calculate_start_params(topology)
+    constraint_val = constraint_thrust_fn(params, model, structure)
 
-    print(f"\nXYZ:")
-    for node in network.nodes():
-        print(f"\tNode {node}: {network.node_coordinates(node)}")
+    print(f"{constraint_val=}")
 
     # Tests
     assert allclose([network.weight()], [network.load_sum_vertical()])
     assert allclose([network.thrust()], [network.load_sum_horizontal()])
-    
+
     # Plotting
     plotter = VaultPlotter(figsize=(8, 8))
-    
+
     plotter.plot_vault(vault, plot_other_half=True)
-    plotter.plot_vault_blocks(vault)
+    # plotter.plot_vault_blocks(vault)
+    plotter.plot_vault_blocks_lines(vault)
 
     # plotter.plot_thrust_network(network)
-    plotter.add(network, show_nodes=True, nodesize=0.5, show_reactions=False, show_loads=False)
-    
     plotter.zoom_extents()
+    plotter.add(
+        network,
+        show_nodes=False,
+        nodesize=0.5,
+        show_reactions=True,
+        reactionscale=0.25,
+        show_loads=False
+    )
+
     plotter.show()
 
     # Optimization
     networks, _ = solve_thrust_minmax_vault(vault, **config["optimization"])
-
-    for network in networks.values():
-        print(f"\nLoads:")
-        for node in network.nodes():
-            print(f"\tNode {node}: qx={network.node_attribute(node, 'qx')}, qy={network.node_attribute(node, 'qy')}")
-
-        print(f"\nXYZ:")
-        for node in network.nodes():
-            print(f"\tNode {node}: {network.node_coordinates(node)}")
-
     plot_thrust_minmax_vault(vault, networks, **config["plotting"])
 
 
