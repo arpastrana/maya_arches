77014bbae66bc7506f53e806a68ee071c6716ed8
Rafael Pastrana <arpj@princeton.edu>
2025-04-12 16:00:17 -0400

    [Plotter] Fixed bug detecting lower constraint at base kink

--- src/mayan_vaults/plotting.py
+++ src/mayan_vaults/plotting.py
@@ -13,6 +13,7 @@ from compas.geometry import Reflection
 from compas.geometry import Point
 from compas.geometry import Vector
 from compas.geometry import Line
+from compas.geometry import Polyline
 from compas.geometry import add_vectors
 from compas.geometry import scale_vector
 from compas.geometry import distance_point_point
@@ -69,12 +70,12 @@ class VaultPlotter(Plotter):
         Plot the vault blocks.
         """
         for i, block in enumerate(vault.blocks.values()):
-            val = i / len(vault.blocks)
+            # val = i / len(vault.blocks)
             self.add(
                 block.polygon(),
-                linewidth=0.0,
+                linewidth=1.0,
                 edgecolor=Color.grey(),
-                facecolor=Color(val, val, 0.5),
+                # facecolor=Color(val, val, 0.5),
                 alpha=0.5,
                 zorder=50,
             )
@@ -87,15 +88,16 @@ class VaultPlotter(Plotter):
             self.add(
                 block.plane_line(),
                 draw_as_segment=True,
-                linestyle="solid",
-                color=Color.from_rgb255(240, 240, 240),  # Color.grey(),
-                lineweight=0.05,
+                linestyle="dashed",
+                color=Color.from_rgb255(240, 240, 240),
+                linewidth=1.0,
                 zorder=100
             )
 
     def plot_thrust_network(
             self,
             network,
+            draw_as_polyline: bool = False,
             linewidth: Union[float, Tuple[float, float]] = 3.0,
             linestyle: str = "solid",
             color: Color = Color.from_rgb255(12, 119, 184),
@@ -120,6 +122,21 @@ class VaultPlotter(Plotter):
             _, end = segment
             points[v] = end
 
+        # Draw the thrust network as a polyline
+        if draw_as_polyline:
+            if not isinstance(linewidth, (int, float)):
+                linewidth = linewidth[0]
+
+            self.add(
+                Polyline(points.values()),
+                draw_points=False, 
+                linestyle=linestyle, 
+                color=color,
+                linewidth=linewidth, 
+                zorder=1000
+                )
+            return
+
         # Calculate line widths
         if isinstance(linewidth, (int, float)):
             edgewidth = {edge: linewidth for edge in network.edges()}
@@ -139,7 +156,7 @@ class VaultPlotter(Plotter):
                 width = (1.0 - ratio) * low + ratio * high
                 edgewidth[edge] = width
 
-        # Draw the thrusts edges
+        # Draw the thrusts edges        
         for edge in network.edges():
             u, v = edge
             start = points.get(u)
@@ -157,6 +174,7 @@ class VaultPlotter(Plotter):
                 linewidth=edgewidth[edge],
                 zorder=1000
             )
+
 
     def plot_thrust_network_loads(self, network, scale: float = 1.0) -> None:
         """
@@ -191,9 +209,10 @@ class VaultPlotter(Plotter):
                 continue
 
             thrust_x = [network.node_attribute(node, 'rx'), 0.0, 0.0]
+            thrust_y = [0.0, network.node_attribute(node, 'ry'), 0.0]
             thrust_xyz = network.node_attributes(node, ['rx', 'ry', 'rz'])
 
-            for thrust in (thrust_xyz, ):
+            for thrust in (thrust_x, thrust_y):
                 thrust_scaled = scale_vector(thrust, scale)
 
                 xyz = network.node_coordinates(node)
@@ -287,9 +306,9 @@ class VaultPlotter(Plotter):
 
             if start.y > end.y:
                 start, end = end, start
-
-            # Check lower bound            
-            if point.y > 0.0 and distance_point_point(point, start) <= tol_constraints:
+        
+            # Check lower bound
+            if point.y > -tol_constraints and distance_point_point(point, start) <= tol_constraints:                
                 self.add(
                     point,
                     size=pointsize,
@@ -299,12 +318,14 @@ class VaultPlotter(Plotter):
 
             # Check upper bound            
             if distance_point_point(point, end) <= tol_constraints:
+                print("Is upper bound")
                 self.add(
                     point,
                     size=pointsize,
                     facecolor=color_constraint_upper,
                     zorder=2000
                 )
+
 
 # ------------------------------------------------------------------------------
 # Artists
@@ -353,17 +374,20 @@ def plot_thrust_minmax_vault(
 
     plotter.zoom_extents()
 
-    for loss_fn_name, network in networks.items():
-        linestyle = "solid" if loss_fn_name == "max" else "solid"
+    for loss_fn_name, network in networks.items():        
+        linestyle = "solid" if loss_fn_name == "max" else "dashed"
         color_blue = Color.from_rgb255(12, 119, 184)
         color_blue_dark = color_blue.darkened(20)
         color = color_blue_dark if loss_fn_name == "max" else color_blue
-
+        draw_as_polyline = loss_fn_name == "min"
+        
         plotter.plot_thrust_network(
             network,
+            draw_as_polyline=draw_as_polyline,
             linestyle=linestyle,
             linewidth=thrust_linewidth,
-            color=color)
+            color=color
+            )
 
         if plot_constraints:
             plotter.plot_constraints(vault, network, tol_bounds, tol_constraints)
@@ -375,19 +399,22 @@ def plot_thrust_minmax_vault(
             plotter.plot_thrust_network_thrusts(network, forcescale)
 
     if save_plot:
+        solution_names = "".join(networks.keys())
         if isinstance(vault, MayaVault):
-            fig_name = f"minmax_maya_h{int(vault.height)}_w{int(vault.width)}_wh{int(vault.wall_height)}_ww{int(vault.wall_width)}_lh{int(vault.lintel_height)}.pdf"
+            fig_name = f"{solution_names}_maya_h{int(vault.height)}_w{int(vault.width)}_wh{int(vault.wall_height * 10.0)}_ww{int(vault.wall_width * 10.0)}_lh{int(vault.lintel_height * 10.0)}_n{int(vault.num_blocks)}.pdf"
         elif isinstance(vault, CircularVault):
-            fig_name = f"minmax_circle_r{int(vault.radius)}_t{int(vault.thickness)}_n{int(vault.num_blocks)}.pdf"        
+            fig_name = f"{solution_names}_circle_r{int(vault.radius)}_t{int(vault.thickness * 10)}_n{int(vault.num_blocks)}.pdf"        
         elif isinstance(vault, EllipticalTaperedVault):
-            fig_name = f"minmax_ellipse_tapered_h{int(vault.height)}_w{int(vault.width)}_tt{int(vault.thickness_top)}_tb{int(vault.thickness_bottom)}_n{int(vault.num_blocks)}.pdf"
+            fig_name = f"{solution_names}_ellipse_tapered_h{int(vault.height)}_w{int(vault.width)}_tt{int(vault.thickness_top * 10)}_tb{int(vault.thickness_bottom * 10)}_n{int(vault.num_blocks)}.pdf"
         elif isinstance(vault, EllipticalVault):
-            fig_name = f"minmax_ellipse_h{int(vault.height)}_w{int(vault.width)}_t{int(vault.thickness)}_n{int(vault.num_blocks)}.pdf"
+            fig_name = f"{solution_names}_ellipse_h{int(vault.height)}_w{int(vault.width)}_t{int(vault.thickness * 10)}_n{int(vault.num_blocks)}.pdf"
         fig_path = os.path.join(FIGURES, fig_name)
         plotter.save(fig_path, transparent=True, bbox_inches="tight")
         print(f"\nSaved figure to {fig_path}")
 
     if show_plot:
         plotter.show()
+    
+    print("\nPlotted!")
 
     plt.close()
